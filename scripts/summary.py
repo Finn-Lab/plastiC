952f8694bad65c7378901b92830131d322f0b2f9
Ellen S Cameron <68868171+escamero@users.noreply.github.com>
2022-10-31 15:09:24 +0000

    change contamination to mitocontam in summary

--- scripts/summary.py
+++ scripts/summary.py
@@ -2,14 +2,14 @@ import pandas as pd
 import os
 import argparse
 
-def summary_table(completeness, contamination, taxonomy, bins):
+def summary_table(completeness, mito_contamination, taxonomy, bins):
     binid = os.listdir(bins)
     completeness = pd.read_csv(completeness, sep = ",")
-    contamination = pd.read_csv(contamination, sep = ",")
+    mito_contamination = pd.read_csv(mito_contamination, sep = ",")
     taxonomy = pd.read_csv(taxonomy, sep = "\t")
 
     taxlineage = taxonomy[["# bin","superkingdom", "phylum", "class", "order", "family", "genus"]].set_index("# bin")
-    quality = completeness.set_index("id").join(contamination.set_index("id"))
+    quality = completeness.set_index("id").join(mito_contamination.set_index("id"))
 
     summary_table = quality.merge(taxlineage, how = 'cross')
     summary_table.insert(loc=0, column='id', value=binid)
@@ -20,13 +20,13 @@ if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument('-comp', '--completeness', required=True, type=str,
                     help='Completeness CSV')
-    ap.add_argument('-cont', '--contamination', required=True, type=str,
-                    help='Contamination CSV')
+    ap.add_argument('-mitocont', '--mitocontamination', required=True, type=str,
+                    help='Mitocondrial Contamination CSV')
     ap.add_argument('-t', '--taxonomy', required=True, type=str, help='Taxonomic classification')
     ap.add_argument('-b', '--bindir', required=True, type=str, help='Bin directory')
     ap.add_argument('-o', '--summaryout', required = True, type=str, help='Summary output file')
     args = ap.parse_args()
 
-    summary = summary_table(args.completeness, args.contamination, args.taxonomy, args.bindir)
+    summary = summary_table(args.completeness, args.mitocontamination, args.taxonomy, args.bindir)
 
     summary.to_csv(args.summaryout, index=False, header=True)
