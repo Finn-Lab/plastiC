352dfd803541bfabf1d1d9218307f8d64621b83e
Ellen S Cameron <68868171+escamero@users.noreply.github.com>
2022-11-01 10:26:13 +0000

    update source classifier to handle no plastid bins

--- scripts/source_classifier.sh
+++ scripts/source_classifier.sh
@@ -48,6 +48,11 @@ do
 
 done
 
-CAT bins -b ${plastidbindir} -d ${databasedir} -t ${taxonomydir} -o ${outputdir}/out.BAT -s .fa --force
+if [`ls -1 ${keggcountdir}/*.csv | wc -l | xargs` -gt 0];
+then
+  CAT bins -b ${plastidbindir} -d ${databasedir} -t ${taxonomydir} -o ${outputdir}/out.BAT -s .fa --force
 
-CAT add_names -i ${outputdir}/out.BAT.bin2classification.txt -o ${outputdir}/out.BAT.plastid_source_taxonomy_predictions.txt -t ${taxonomydir} --only_official --exclude_scores
+  CAT add_names -i ${outputdir}/out.BAT.bin2classification.txt -o ${outputdir}/out.BAT.plastid_source_taxonomy_predictions.txt -t ${taxonomydir} --only_official --exclude_scores
+else
+  touch ${outputdir}/out.BAT.plastid_source_taxonomy_predictions.txt
+fi
